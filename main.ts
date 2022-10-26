4334e967a3c0ad55ca6114abcb480034a7e520a1
roomkibou <113073945+roomkibou@users.noreply.github.com>
2022-10-26 17:04:14 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,30 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    music.playTone(262, music.beat(BeatFraction.Whole))
-    basic.showIcon(IconNames.Heart)
-    basic.pause(500)
+    if (true) {
+        basic.showIcon(IconNames.Heart)
+        music.playTone(880, music.beat(BeatFraction.Whole))
+        basic.showNumber(kyoudo)
+    }
 })
 input.onButtonPressed(Button.A, function () {
-	
+    if (kyoudo > 0) {
+        kyoudo += -1
+        basic.showNumber(kyoudo)
+    }
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.House)
+    music.playTone(440, music.beat(BeatFraction.Eighth))
+    radio.setTransmitPower(kyoudo)
+    radio.sendString("takara")
+    basic.showNumber(kyoudo)
+})
+input.onButtonPressed(Button.B, function () {
+    if (kyoudo < 7) {
+        kyoudo += 1
+        basic.showNumber(kyoudo)
+    }
+})
+let kyoudo = 0
 radio.setGroup(1)
-radio.sendNumber(0)
-radio.setTransmitPower(7)
-basic.showIcon(IconNames.Yes)
-basic.forever(function () {
-	
-})
+kyoudo = 7
+basic.showNumber(kyoudo)
